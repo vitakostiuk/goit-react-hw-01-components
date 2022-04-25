e306632d0e1b0352126e11fec86635cc1422d9e4
Vita Kostiuk <vitagrebennik@gmail.com>
2022-04-25 18:53:51 +0300

    task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,18 @@
+import Profile from './Profile/Profile';
+import user from './Profile/user';
+
+console.log(user);
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
